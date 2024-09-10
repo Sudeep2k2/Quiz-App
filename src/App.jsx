e89d4ddd3dbe6e9c1ab67ab3b51b463f52667bcc
Sudeep2k2 <rockingsudeep08@gmail.com>
2024-09-10 22:13:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Quiz } from './Components/Quiz Components/Quiz'
+
+export const App = () => {
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
---
+++ src/Components/Quiz Components/Quiz.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Quiz.css';
+import { data } from '../../assets/data';
+import { useState } from 'react';
+
+export const Quiz = () => {
+  
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+
+    return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr/>
+        <h2>{index+1}. {question.question}</h2>
+        <ul>
+            <li>{question.option1}</li>
+            <li>{question.option2}</li>
+            <li>{question.option3}</li>
+            <li>{question.option4}</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 Questions</div>
+    </div>
+  )
+}
